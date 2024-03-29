87347cd7ca57d6591283efe12c250d89aa9b55ec
Wallysson <wally.nm@gmail.com>
2024-03-29 18:35:03 -0300

    Updates config for svelte

--- src/lib/config.js
+++ src/lib/config.js
@@ -5,11 +5,11 @@
  * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
  **/ 
 
-export const siteTitle = 'My Awesome Blog'
+export const siteTitle = 'Wallynm Blog'
 export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
-export const siteURL = 'example.com'
-export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = '- find and change this text in src/lib/config.js'
+export const siteURL = 'wallynm.dev'
+export const siteLink = 'https://wallynm.dev/'
+export const siteAuthor = 'Wallysson Nunes da mota'
 
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
