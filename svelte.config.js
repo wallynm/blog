d5087d676b1bd1ae97d7e92d7e108d3e2b9fd1e9
Wallysson <wally.nm@gmail.com>
2024-03-29 16:17:22 -0300

    Adds initial blog files

---
+++ svelte.config.js
@@ -0,0 +1,56 @@
+import adapter from '@sveltejs/adapter-static'
+import { mdsvex } from 'mdsvex'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		mdsvex({
+			// The default mdsvex extension is .svx; this overrides that.
+			extensions: ['.md'],
+
+			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+			rehypePlugins: [
+				rehypeExternalLinks,
+				rehypeSlug,
+				rehypeAutolinkHeadings,
+				{
+					// Adds hyperlinks to the headings, requires rehypeSlug
+					behavior: 'prepend',
+					properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
+					content: {
+						type: 'element',
+						tagName: 'span',
+						properties: {
+							className: ['rehypeAutolinkHeadings'],
+						},
+						children: [{ type: 'text', value: '#' }]
+					}
+				}
+			],
+		}),
+	],
+
+	kit: {
+		adapter: adapter(),
+		prerender: {
+		entries: [
+			'*',
+			'/api/posts/page/*',
+			'/blog/category/*/page/',
+			'/blog/category/*/page/*',
+			'/blog/category/page/',
+			'/blog/category/page/*',
+			'/blog/page/',
+			'/blog/page/*',
+		]
+		}
+	}
+};
+
+export default config;
